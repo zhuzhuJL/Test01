57325929212c00c27e0c06550d679ad0c16012fb
zhuzhuJL <2293701870@qq.com>
2019-11-07 09:46:35 +0800

    Initial commit

---
+++ src/com/test/java/HelloWord.java
@@ -0,0 +1,15 @@
+package com.test.java;
+
+public class HelloWord {
+   private Integer num;
+   private String name;
+
+    public Integer getNum() {
+        return num;
+    }
+
+    //单行注释
+    public static void main(String[] srgs){
+          System.out.print("helloword!!");
+    }
+}
---
+++ src/com/test/java/Hellow.java
@@ -0,0 +1,20 @@
+package com.test.java;
+
+/**
+ * @author zhuzhu
+ * @date 2019/11/5 -19:58
+ */
+public class Hellow {
+    //模板一：pswm
+    public static void main(String[] args) {
+
+        //模板二：sout
+        System.out.println("hellow");
+        //变形：soutp/soutm/soutv/xxx.sout
+        System.out.println("args = [" + args + "]");
+        System.out.println("Hellow.main");
+        int num1=10;
+        System.out.println("num1 = " + num1);
+    }
+
+}
